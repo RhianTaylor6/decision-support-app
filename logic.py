fd2db37726750b5f8da2078776951acb2ddbab64
Rhian <rhiant413@gmail.com>
2023-12-16 20:17:01 +0000

    improving logic

--- logic.py
+++ logic.py
@@ -26,4 +26,21 @@ for i in range(int(num_lists)):
 
     my_lists[cat_key] = options_list
 
-print(my_lists)
+def make_choice():
+    for category, options in my_lists.items():
+        choice = random.randint(0,len(options)-1)
+        print(category +":"+options[choice])
+
+print("Here are the choices we have generated for you:")
+make_choice()
+
+print("Are you happy with these choices? (yes or no)")
+happy = input()
+while happy == "no" or happy == "No":
+    print("Here are some alternative choices we have generated for you:")
+    make_choice()
+    print("Are you happy with these choices? (yes or no)")
+    happy = input()
+    print("Enjoy3!")
+
+#print(my_lists)
