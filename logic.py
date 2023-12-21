4e9ed2cf5becb7d679a53ae30ffc911e4ac59ca1
Rhian Taylor <L252744@d04jgvm0j9.ema.lilly.com>
2023-12-21 15:58:11 +0000

    trying to add save and read from file

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 #DEFINE FUNCTIONS/METHODS - could split these out into seperate file
 
@@ -39,41 +40,70 @@ def make_choice(all):
                     choice = random.randint(0,len(options)-1)
                     print(category +" : "+options[choice])
 
-#MAIN BODY OF CODE
+def write_dic(dic):
+    json = json.dumps(dic)
+    f = open("categories.json","w")
+    f.write(json)
+    f.close()
 
-# give user facility to input how many decisions they want help with and save as integer
-print("How many decisions do you need to make?")
-num_lists = input()
-my_lists = {}
+def load_dic():
+    f = open("categories.json","r")
+    dic = json.load(f)
+    return dic
 
-#give user a way to input names of categories for the number of decisions they want help with
-#add categories to my_lists
-for i in range(int(num_lists)):
-    print("Please name the decision category "+str(i+1)+":")
-    cat_key = input()
-    
-    print("Please add an option for "+cat_key+":")
-    cat_option = input()
-    
-    #create list and add cat_option to the list
-    options_list = []
-    options_list.append(cat_option)
-   
-    print("Do you want to add another option for "+cat_key+": (yes or no)")
-    more_options = input().lower().strip()
-    more_options = check_y_n(more_options)
-    
-    #check if user wants to add any more options and allow input if true
-    while more_options == "yes" or more_options =="Yes":
+
+
+def new_cats():
+    # give user facility to input how many decisions they want help with and save as integer
+    print("How many decisions do you need to make?")
+    num_lists = input()
+    my_lists = {}
+
+    #give user a way to input names of categories for the number of decisions they want help with
+    #add categories to my_lists
+    for i in range(int(num_lists)):
+        print("Please name the decision category "+str(i+1)+":")
+        cat_key = input()
+        
         print("Please add an option for "+cat_key+":")
         cat_option = input()
+        
+        #create list and add cat_option to the list
+        options_list = []
         options_list.append(cat_option)
+    
         print("Do you want to add another option for "+cat_key+": (yes or no)")
         more_options = input().lower().strip()
         more_options = check_y_n(more_options)
+        
+        #check if user wants to add any more options and allow input if true
+        while more_options == "yes" or more_options =="Yes":
+            print("Please add an option for "+cat_key+":")
+            cat_option = input()
+            options_list.append(cat_option)
+            print("Do you want to add another option for "+cat_key+": (yes or no)")
+            more_options = input().lower().strip()
+            more_options = check_y_n(more_options)
 
-    #write key:value pairs into the my_lists dictionary
-    my_lists[cat_key] = options_list
+        #write key:value pairs into the my_lists dictionary
+        my_lists[cat_key] = options_list
+        write_dic(my_lists)
+        return my_lists
+    
+#MAIN BODY OF CODE
+print("Would you like to load previously defined categories? (yes/no)")
+load_choice=input().lower().strip()
+load_choice = check_y_n(all)
+
+if load_choice == "yes":
+    try:
+        my_lists = load_dic()
+    except:
+        print("no file to load, please define categories.")
+        new_cats()
+else:
+    new_cats()
+
 
 #ask if user wants to generate for all or a subset
 print("Would you like to generate options for all categories? (yes/no)")
@@ -87,13 +117,14 @@ if all == "no":
     #need to validate user input
     picked = input()
     pick_cat.append(picked)
-
+   
     #ask if they want to pick another category
     print("Do you want to generate options for another category (yes or no)")
     more_options = input().lower().strip()
     more_options = check_y_n(more_options)
     
-    while more_options == "yes" or more_options =="Yes":
+    
+    while more_options == "yes":
         print("Please choose a category from the below to generate a choice: \n" + str(my_lists.keys()))
         #need to validate user input
         picked = input()
